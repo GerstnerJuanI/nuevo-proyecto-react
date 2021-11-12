e752592a1f76284db7e586ec7f44927be3f08811
GerstnerJuanI <43797469+GerstnerJuanI@users.noreply.github.com>
2021-11-12 11:28:08 -0300

    Nuevo Proyecto React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [linea1,setLinea1] = useState();
+  const [linea2,setLinea2] = useState();
+  const [imagen, setImagen] = useState();
+
+  const onChangeLinea1 = function (evento) {
+    setLinea1(evento.target.value);
+  }
+  const onChangeLinea2 = function (evento) {
+    setLinea2(evento.target.value);
+  }
+  const onChangeImagen = function (evento) {
+    setImagen(evento.target.value);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* --select picker de memes */}
+      <select onChange={onChangeImagen}>
+        <option value="Casa en llamas"  >Casa en llamas</option>
+        <option value="Futurama">Futurama</option>
+        <option value="History channel">History channel</option>
+        <option value="Matrix">Matrix</option>
+        <option value="Philosoraptor">Philosoraptor</option>
+        <option value="Smart Guy">Smart Guy</option>
+      </select><br />
+      <input onChange={onChangeLinea1} type="text" placeholder="Linea 1" /> <br />
+      <input onChange={onChangeLinea2} type="text" placeholder="Linea 2" /> <br />
+      <button>Exportar</button>
+      <div>
+        <span>{linea1}</span>
+        <span>{linea2}</span>
+        <img src="" alt="" />
+      </div>
     </div>
   );
 }
